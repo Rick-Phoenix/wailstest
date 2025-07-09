a0caf9207834d331889a2804fda2a789289bb5d3
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-09 18:12:41 +0200

    Added shadcn sidebar

---
+++ frontend/src/lib/components/app-sidebar.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import HouseIcon from "@lucide/svelte/icons/house";
+  import InboxIcon from "@lucide/svelte/icons/inbox";
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import { useSidebar } from "$lib/components/ui/sidebar/index.js";
+
+  const sidebar = useSidebar();
+
+  const items = [
+    {
+      title: "Home",
+      url: "#",
+      icon: HouseIcon,
+    },
+    {
+      title: "Inbox",
+      url: "#",
+      icon: InboxIcon,
+    },
+  ];
+</script>
+
+<Sidebar.Root
+  variant="inset"
+  collapsible="icon"
+  onmouseenter={() => {
+    sidebar.setOpen(true);
+  }}
+  onmouseleave={() => {
+    sidebar.setOpen(false);
+  }}
+>
+  <Sidebar.Content>
+    <Sidebar.Group>
+      <Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+      <Sidebar.GroupContent>
+        <Sidebar.Menu>
+          {#each items as item (item.title)}
+            <Sidebar.MenuItem>
+              <Sidebar.MenuButton>
+                {#snippet child({ props })}
+                  <a href={item.url} {...props}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                {/snippet}
+              </Sidebar.MenuButton>
+            </Sidebar.MenuItem>
+          {/each}
+        </Sidebar.Menu>
+      </Sidebar.GroupContent>
+    </Sidebar.Group>
+  </Sidebar.Content>
+</Sidebar.Root>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import AppSidebar from "$lib/components/app-sidebar.svelte";
   import "../app.css";
 
   let { children } = $props();
 </script>
 
-{@render children()}
+<Sidebar.Provider>
+  <AppSidebar />
+  <Sidebar.Inset>
+    <main>
+      <Sidebar.Trigger />
+      {@render children?.()}
+    </main>
+  </Sidebar.Inset>
+</Sidebar.Provider>
