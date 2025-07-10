b94b9e36200bb2f22dd449a2e6059d3586512d4b
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-10 16:49:18 +0200

    Added mode-watcher

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { ModeWatcher } from "mode-watcher";
   import * as Sidebar from "$lib/components/ui/sidebar/index.js";
   import AppSidebar from "$lib/components/app-sidebar.svelte";
   import "../app.css";
@@ -6,6 +7,7 @@
   let { children } = $props();
 </script>
 
+<ModeWatcher defaultTheme="dark" />
 <Sidebar.Provider>
   <AppSidebar />
   <Sidebar.Inset>
--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -1,10 +1,43 @@
 <script lang="ts">
   import { useSidebar } from "$lib/components/ui/sidebar";
+  import SunIcon from "@lucide/svelte/icons/sun";
+  import MoonIcon from "@lucide/svelte/icons/moon";
+
+  import { toggleMode } from "mode-watcher";
+  import { Button } from "$lib/components/ui/button/index.js";
 
   const sidebar = useSidebar();
+
+  let openOnHoverChecked = $state(sidebar.openOnHover);
+
+  const toggleOpenOnHover = () => {
+    if (sidebar.open) {
+      sidebar.toggle();
+    }
+    setTimeout(() => {
+      sidebar.setOpenOnHover(openOnHoverChecked);
+    }, 0);
+  };
 </script>
 
 <label>
-  <input type="checkbox" bind:checked={sidebar.openOnHover} />
+  <input
+    type="checkbox"
+    bind:checked={openOnHoverChecked}
+    onclick={toggleOpenOnHover}
+  />
   Open sidebar on hover
 </label>
+
+<div class="flex gap-3 p-3 items-center">
+  <span>Toggle theme</span>
+  <Button onclick={toggleMode} variant="outline" size="icon">
+    <SunIcon
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
+    />
+    <MoonIcon
+      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
+    />
+    <span class="sr-only">Toggle theme</span>
+  </Button>
+</div>
