a09a40ba77e33ce2cce3f5d809858b58290aae12
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-10 19:22:21 +0200

    Added daisyui and iconify as plugins

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import { useSidebar } from "$lib/components/ui/sidebar";
-  import SunIcon from "@lucide/svelte/icons/sun";
-  import MoonIcon from "@lucide/svelte/icons/moon";
 
   import { toggleMode } from "mode-watcher";
   import { Button } from "$lib/components/ui/button/index.js";
@@ -20,24 +18,29 @@
   };
 </script>
 
-<label>
-  <input
-    type="checkbox"
-    bind:checked={openOnHoverChecked}
-    onclick={toggleOpenOnHover}
-  />
-  Open sidebar on hover
-</label>
+<div class="p-3">
+  <h1 class="p-3 text-lg">Settings</h1>
+  <label>
+    <input
+      type="checkbox"
+      class="checkbox"
+      bind:checked={openOnHoverChecked}
+      onclick={toggleOpenOnHover}
+    />
+    Open sidebar on hover
+  </label>
 
-<div class="flex gap-3 p-3 items-center">
-  <span>Toggle theme</span>
-  <Button onclick={toggleMode} variant="outline" size="icon">
-    <SunIcon
-      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
-    />
-    <MoonIcon
-      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
-    />
-    <span class="sr-only">Toggle theme</span>
-  </Button>
+  <div class="flex gap-3 p-3 items-center">
+    <span>Toggle theme</span>
+    <Button onclick={toggleMode} variant="outline" size="icon">
+      <span
+        class="icon-[lucide--sun] h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
+      ></span>
+
+      <span
+        class="icon-[lucide--moon] absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
+      ></span>
+      <span class="sr-only">Toggle theme</span>
+    </Button>
+  </div>
 </div>
