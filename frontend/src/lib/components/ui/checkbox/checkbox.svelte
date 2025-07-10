d902bf6dd1d14037ebe2aa9a395aa31567fb6482
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-10 23:58:16 +0200

    Added checkbox

---
+++ frontend/src/lib/components/ui/checkbox/checkbox.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { cn } from "$lib/utils";
+  import type { HTMLInputAttributes } from "svelte/elements";
+
+  let {
+    class: className,
+    defaultChecked,
+    checked = $bindable(defaultChecked),
+    indeterminate = $bindable(false),
+    ...restProps
+  }: HTMLInputAttributes = $props();
+</script>
+
+<input
+  type="checkbox"
+  class={cn("checkbox not-checked:bg-muted checked:bg-accent", className)}
+  bind:checked
+  bind:indeterminate
+  {...restProps}
+/>
